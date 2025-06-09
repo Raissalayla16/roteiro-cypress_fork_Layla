b274cb3789a0ce6bce689ffa3c80ff86c85b1e45
Raissalayla16 <raissalayla16@gmail.com>
2025-06-08 23:53:03 -0300

    TestesmE2E

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -69,3 +69,121 @@ describe('TODOMvc App', () => {
       .should('have.length', 2);
   });
 });
+
+
+describe('TODOMvc App', () => {
+  it('Verifica se app está abrindo', () => {
+    cy.visit('')
+  })
+
+  it('Insere uma tarefa', () => {
+    cy.visit(''); 
+
+    cy.get('[data-cy=todo-input]')
+      .type('TP2 de Engenharia de Software{enter}');
+
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 1) 
+      .first()
+      .should('have.text', 'TP2 de Engenharia de Software'); 
+  });
+
+  it('Insere e deleta uma tarefa', () => {
+    cy.visit('');
+
+    cy.get('[data-cy=todo-input]')
+      .type('TP2 de Engenharia de Software{enter}');
+
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 1);
+
+    cy.get('[data-cy=todos-list] > li [data-cy=remove-todo-btn]')
+      .invoke('show')
+      .click();
+
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 0);
+  });
+
+  it('Filtra tarefas completas e ativas', () => {
+    cy.visit(''); 
+
+    cy.get('[data-cy=todo-input]')
+      .type('TP2 de ES{enter}')
+      .type('Prova de ES{enter}');
+
+    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+      .first()
+      .click();
+
+    cy.get('[data-cy=filter-active-link')
+      .click();
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 1)
+      .first()
+      .should('have.text', 'Prova de ES');
+
+    cy.get('[data-cy=filter-completed-link')
+      .click();
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 1)
+      .first()
+      .should('have.text', 'TP2 de ES');
+
+    cy.get('[data-cy=filter-all-link')
+      .click();
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 2);
+  });
+
+  it('Edita uma tarefa existente', () => {
+    cy.visit('');
+
+    cy.get('[data-cy=todo-input]')
+      .type('Estudar Cypress{enter}');
+
+    cy.get('[data-cy=todos-list] li')
+      .dblclick();
+
+    cy.get('[data-cy=todos-list] li input.edit')
+      .clear()
+      .type('Estudar Cypress com testes{enter}');
+
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .first()
+      .should('have.text', 'Estudar Cypress com testes');
+  });
+
+  it('Marca uma tarefa como concluída', () => {
+    cy.visit('');
+
+    cy.get('[data-cy=todo-input]')
+      .type('Estudar para TP2{enter}');
+
+    cy.get('[data-cy=todos-list] li')
+      .first()
+      .find('[data-cy=toggle-todo-checkbox]')
+      .check()
+      .should('be.checked');
+  });
+
+  it('Marca uma tarefa como concluída e verifica que está marcada', () => {
+    cy.visit('');
+
+    cy.get('[data-cy=todo-input]')
+      .type('Fazer relatório do TP2{enter}');
+
+    cy.get('[data-cy=todos-list] li')
+      .first()
+      .find('[data-cy=toggle-todo-checkbox]')
+      .check()
+      .should('be.checked');
+  });
+});
